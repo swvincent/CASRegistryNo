a253039a3aa7d31b31c98d27014557ffa0b7e603
Scott W. Vincent <scott@swvincent.com>
2019-03-04 14:42:05 -0500

    Add Unit Tests

---
+++ src/CASRegistryNo.UnitTests/CasRegNoTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace CASRegistryNo.UnitTests
+{
+    [TestFixture]
+    public class CasRegNoTests
+    {
+        [Test]
+        public void Validate_ValidCasRegNo_ReturnsTrue()
+        {
+            var result = CasRegNo.Validate("58-08-2");
+
+            Assert.That(result, Is.EqualTo((true, "")));
+        }
+
+        [Test]
+        [TestCase("5808-2")]    // Wrong format
+        [TestCase("58-08-3")]   // Bad checksum
+        public void Validate_InvalidCasRegNo_ReturnsFalseAndErrorMessage(string casRegNo)
+        {
+            var result = CasRegNo.Validate(casRegNo);
+
+            Assert.That(!result.isValid && !string.IsNullOrWhiteSpace(result.errorMessage));
+        }
+    }
+}
