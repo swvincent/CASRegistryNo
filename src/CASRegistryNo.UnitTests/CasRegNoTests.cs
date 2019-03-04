dfc36a387768dbef84e11b86df9fcf833f790232
Scott W. Vincent <scott@swvincent.com>
2019-03-04 16:08:07 -0500

    Set names for Test cases

--- src/CASRegistryNo.UnitTests/CasRegNoTests.cs
+++ src/CASRegistryNo.UnitTests/CasRegNoTests.cs
@@ -14,9 +14,9 @@ namespace CASRegistryNo.UnitTests
         }
 
         [Test]
-        [TestCase("5808-2")]    // Wrong format
-        [TestCase("58-08-3")]   // Bad checksum
-        [TestCase("")]          // No CAS registry number
+        [TestCase("5808-2", TestName = "Validate_CasRegNoWrongFormat_ReturnsFalseAndErrorMessage")]
+        [TestCase("58-08-3", TestName = "Validate_CasRegNoBadChecksum_ReturnsFalseAndErrorMessage")]
+        [TestCase("", TestName = "Validate_CasRegNoEmpty_ReturnsFalseAndErrorMessage")]
         public void Validate_InvalidCasRegNo_ReturnsFalseAndErrorMessage(string casRegNo)
         {
             var result = CasRegNo.Validate(casRegNo);
